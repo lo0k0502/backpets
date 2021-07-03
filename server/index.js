5f58a4d94d35695b96e02b7933eb840a2eebd8f7
lo0k0502 <thomas85514@gmail.com>
2021-07-04 04:24:17 +0800

    connect to mongodb

--- server/index.js
+++ server/index.js
@@ -6,4 +6,17 @@ dotenv.config();
 
 const port = process.env.PORT || 8001;
 
-app.listen(port, () => console.log(`server is running on ${port}`));
+mongoose
+    .connect(process.env.DB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .catch((err) => {
+        console.log(err.stack);
+        process.exit(1);
+    })
+    .then(async (client) => {
+        app.listen(port, () => console.log(`server is running on ${port}`));
+    });
+
+mongoose.connection.on("connected", () => console.log("db connected"));
