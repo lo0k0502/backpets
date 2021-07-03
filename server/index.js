eaf60515f5019863e8fdc9d919dcb8cf2a988ee7
lo0k0502 <thomas85514@gmail.com>
2021-07-04 02:49:08 +0800

    initialize server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,9 @@
-import express from "express";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import app from './server.js';
+
+dotenv.config();
+
+const port = process.env.PORT || 8001;
+
+app.listen(port, () => console.log(`server is running on ${port}`));
