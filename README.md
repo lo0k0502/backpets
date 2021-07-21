76a78988cdc4fc25e21cd7d1db6b7e6418cb1766
lo0k0502 <thomas85514@gmail.com>
2021-07-21 15:04:34 +0800

    fix multer-gridfs-storage error

--- README.md
+++ README.md
@@ -13,3 +13,11 @@ cd server
 yarn install
 yarn start
 ```
+
+## Node Module Error
+Make sure to change the mongodb_1.ObjectID() inside the multer-gridfs-storage module's gridfs.js file into mongodb_1.ObjectId() to avoid error.
+```bash
+124         if (!hasId) {
+125             previous.id = new mongodb_1.ObjectID();//Change into mongodb_1.ObjectId()
+126         }
+```
--- server/middleware/uploadimg.js
+++ server/middleware/uploadimg.js
@@ -1,13 +1,17 @@
 import multer from 'multer';
-import GridFsStorage from 'multer-gridfs-storage';
+import { GridFsStorage } from 'multer-gridfs-storage';
 import moment from 'moment';
+import mongoose from 'mongoose';
 
 const storage = new GridFsStorage({
     url: process.env.DB_URI,
-    file: (req, file) => {
-        return { filename:`${moment().valueOf()}-${file.originalname}` };
+    options: {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
     },
-    root: 'avatar',
+    file: (req, file) => {
+        return { filename:`${moment().valueOf()}-${file.originalname}`, bucketName: 'avatar' };
+    },
 });
 
 const uploadimg = multer({ storage }).single('avatar');
