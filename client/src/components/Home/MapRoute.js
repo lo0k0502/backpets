b8495a7c07e0334e3d68ce514949532f5fc537d0
lo0k0502 <thomas85514@gmail.com>
2021-10-24 00:40:53 +0800

    same as previous one

--- client/src/components/Home/MapRoute.js
+++ client/src/components/Home/MapRoute.js
@@ -13,10 +13,13 @@ export default () => {
 
   useFocusEffect(useCallback(() => {
   	(async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-        return;
+      let currentPermission = await Location.getForegroundPermissionsAsync();
+      if (currentPermission.status !== 'granted') {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+          setErrorMsg('Permission to access location was denied');
+          return;
+        }
       }
 
       let location = await Location.getCurrentPositionAsync({});
